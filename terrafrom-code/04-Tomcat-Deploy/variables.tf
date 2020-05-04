ffe6c380d8ac0c77d157c0518c3ab3386441f343
deekshar <deekshaar65@gmail.com>
2020-05-04 20:31:35 +0530

    Update variables.tf

--- terrafrom-code/04-Tomcat-Deploy/variables.tf
+++ terrafrom-code/04-Tomcat-Deploy/variables.tf
@@ -1,15 +1,15 @@
 variable "key_name" {
-  default = "mykey-test-1"
+  default = "awskeypair"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/amit-personal-aws.pem"
+  default = "/root/.ssh/awskey.pem"
 }
 
 variable "us-east-zones" {
-  default = ["us-east-1a", "us-east-1b"]
+  default = ["us-east-2a", "us-east-2b"]
 }
 
 variable "sg-id" {
-  default = "sg-0eec038018ea2902d"
+  default = "sg-2454445d"
 }
